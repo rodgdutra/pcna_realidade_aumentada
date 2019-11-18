5d954c52decfc94688f27d79282810fb2f797242
Rodrigo Gomes Dutra <dutra.rgdgd@gmail.com>
2019-11-18 19:48:17 -0300

    adicionado 3 ondas animadas

--- senoide/Assets/Graph_result.cs
+++ senoide/Assets/Graph_result.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 /// Draws a sine wave using a line renderer
 /// </summary>
 
-public class Graph : MonoBehaviour {
+public class Graph_result : MonoBehaviour {
 
     public float min_x = (float)-0.5;
     public float max_x = (float)0.5;
@@ -34,7 +34,19 @@ public class Graph : MonoBehaviour {
 
     //Calculate graph and set points on line renderer
     void DrawGraph()
-    {   
+    {   // Get the 2 waves parameters
+        float wave1_height    = GameObject.Find("onda_1").GetComponent<Graph>().height;
+        float wave1_phase     = GameObject.Find("onda_1").GetComponent<Graph>().phase;
+        float wave1_frequency = GameObject.Find("onda_1").GetComponent<Graph>().frequency;
+
+        float wave2_height    = GameObject.Find("onda_2").GetComponent<Graph>().height;
+        float wave2_phase     = GameObject.Find("onda_2").GetComponent<Graph>().phase;
+        float wave2_frequency = GameObject.Find("onda_2").GetComponent<Graph>().frequency;
+
+        float wave1_z = 0;
+        float wave2_z = 0;
+
+        // x increment calculation
         float x_increment = ((min_x*-1) + max_x)/(resolution);
 
         positions = new Vector3[resolution];
@@ -43,14 +55,18 @@ public class Graph : MonoBehaviour {
         // Position 0
         positions[0].x = min_x;
         positions[0].y = 0;
-        positions[0].z = height * Mathf.Sin(2*3);
+        wave1_z = wave1_height * Mathf.Sin(2 * 3 * wave1_frequency + wave1_phase);
+        wave2_z = wave2_height * Mathf.Sin(2*3*wave2_frequency*+wave2_phase);
+        positions[0].z = wave1_z+ wave1_z;
 
         for (int index = 1; index < positions.Length; ++index)
         {
             // Update the positions by each index 
             positions[index].x = positions[index-1].x + x_increment;
             positions[index].y = 0;
-            positions[index].z = height * Mathf.Sin(2 * 3 * frequency * index + time);
+            wave1_z = wave1_height * Mathf.Sin(2 * 3 * wave1_frequency * index + wave1_phase + time);
+            wave2_z = wave2_height * Mathf.Sin(2 * 3 * wave2_frequency * index + wave2_phase + time);
+            positions[index].z = wave1_z + wave2_z;
 
         }
 
